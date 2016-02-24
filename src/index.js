c248c1483954a25dcc7c56aeae0a7ea5ac93d4be
William Cotton <williamcotton@gmail.com>
2016-02-24 07:23:25 -0800

    v2.0.0 - added support for 80 byte max

--- src/index.js
+++ src/index.js
@@ -1,110 +1,113 @@
-var Bitcoin = require("bitcoinjs-lib");
+var bitcoin = require('bitcoinjs-lib')
 
-var createSignedTransactionWithData = function(options, callback) {
-  var commonBlockchain = options.commonBlockchain;
-  var commonWallet = options.commonWallet;
-  var data = options.data;
-  if (data.length > 40) {
-    callback("too large", false);
-    return;
-  };
-  var address = commonWallet.address;
-  var fee = options.fee || 1000;
-  var payloadScript = Bitcoin.Script.fromChunks([Bitcoin.opcodes.OP_RETURN, data]);
-  var tx = new Bitcoin.TransactionBuilder();
-  commonBlockchain.Addresses.Unspents([address], function(err, addresses_unspents) {
-    var unspentOutputs = addresses_unspents[0];
-    var compare = function(a,b) {
-      if (a.value < b.value)
-        return -1;
-      if (a.value > b.value)
-        return 1;
-      return 0;
-    };
-    unspentOutputs.sort(compare);
-    var unspentValue = 0;
+var createSignedTransactionWithData = function (options, callback) {
+  var commonBlockchain = options.commonBlockchain
+  var commonWallet = options.commonWallet
+  var data = options.data
+  if (data.length > 80) {
+    callback('too large', false)
+    return
+  }
+  var address = commonWallet.address
+  var fee = options.fee || 1000
+  var payloadScript = bitcoin.script.nullDataOutput(data)
+  var tx = new bitcoin.TransactionBuilder(bitcoin.networks[commonWallet.network])
+  commonBlockchain.Addresses.Unspents([address], function (err, addresses_unspents) {
+    if (err) {
+      return callback(err, false)
+    }
+    var unspentOutputs = addresses_unspents[0]
+    var compare = function (a, b) {
+      if (a.value < b.value) {
+        return -1
+      }
+      if (a.value > b.value) {
+        return 1
+      }
+      return 0
+    }
+    unspentOutputs.sort(compare)
+    var unspentValue = 0
     for (var i = unspentOutputs.length - 1; i >= 0; i--) {
-      var unspentOutput = unspentOutputs[i];
+      var unspentOutput = unspentOutputs[i]
       if (unspentOutput.value === 0) {
-        continue;
+        continue
       }
-      unspentValue += unspentOutput.value;
-      tx.addInput(unspentOutput.txid, unspentOutput.vout);
+      unspentValue += unspentOutput.value
+      tx.addInput(unspentOutput.txid, unspentOutput.vout)
       if (unspentValue - fee >= 0) {
-        break;
+        break
       }
-    };
-    tx.addOutput(payloadScript, 0);
-    tx.addOutput(address, unspentValue - fee);
-    var txHex = tx.tx.toHex();
-    commonWallet.signRawTransaction(txHex, callback);
-  });
-};
+    }
+    tx.addOutput(payloadScript, 0)
+    tx.addOutput(address, unspentValue - fee)
+    var txHex = tx.tx.toHex()
+    commonWallet.signRawTransaction(txHex, callback)
+  })
+}
 
-var getDatum = function(options, callback) {
-  var transactions = options.transactions;
-  var datum = [];
-  transactions.forEach(function(tx) {
-    tx.vout.forEach(function(output) {
-      if (output.scriptPubKey.type == 'nulldata') {
-        var scriptPubKey = output.scriptPubKey.hex;
-        if (scriptPubKey.slice(0,2) == "6a") {
-          var data = scriptPubKey.slice(4, 84);
-          var bufferData = new Buffer(data, "hex");
-          datum.push(bufferData);
+var getDatum = function (options, callback) {
+  var transactions = options.transactions
+  var datum = []
+  transactions.forEach(function (tx) {
+    tx.vout.forEach(function (output) {
+      if (output.scriptPubKey.type === 'nulldata') {
+        var scriptPubKeyASM = output.scriptPubKey.asm
+        if (scriptPubKeyASM.indexOf('OP_RETURN ') === 0) {
+          var data = scriptPubKeyASM.split('OP_RETURN ')[1]
+          var bufferData = new Buffer(data, 'hex')
+          datum.push(bufferData)
         }
       }
-    });
-  });
+    })
+  })
   callback(false, datum)
-};
+}
 
-var post = function(options, callback) {
-  var data;
+var post = function (options, callback) {
+  var data
   if (options.stringData) {
-    data = new Buffer(options.stringData);
+    data = new Buffer(options.stringData)
+  } else if (options.data) {
+    data = options.data
+  } else if (options.hexData) {
+    data = new Buffer(options.hexData, 'hex')
   }
-  else if (options.data) {
-    data = options.data;
-  }
-  else if (options.hexData) {
-    data = new Buffer(options.hexData, "hex");
-  }
-  if (data.length > 40) {
-    return callback("too large", false);
+  if (data.length > 80) {
+    return callback('too large', false)
   }
   if (data.length < 1) {
-    return callback("too small", false);
+    return callback('too small', false)
   }
-  var commonBlockchain = options.commonBlockchain;
-  var commonWallet = options.commonWallet;
-  var propagateTransaction = options.propagateTransaction;
-  options.data = data;
-  createSignedTransactionWithData(options, function(err, signedTxHex, txid) {
-    var propagateResponse = function(err, res) {
+  var commonBlockchain = options.commonBlockchain
+  options.data = data
+  createSignedTransactionWithData(options, function (err, signedTxHex, txid) {
+    if (err) {
+      return callback(err, false)
+    }
+    var propagateResponse = function (err, res) {
       var postTx = {
         data: options.data,
         txid: txid
       }
       if (err) {
-        postTx.propagateResponse = "failure";
+        postTx.propagateResponse = 'failure'
+      } else {
+        postTx.propagateResponse = 'success'
       }
-      else {
-        postTx.propagateResponse = "success";
-      }
-      callback(err, postTx);
+      callback(err, postTx)
     }
-    commonBlockchain.Transactions.Propagate(signedTxHex, propagateResponse);
-  });
-};
+    commonBlockchain.Transactions.Propagate(signedTxHex, propagateResponse)
+  })
+}
 
-var scan = function(tx, callback) {
-  getDatum({transactions:[tx]}, function(err, txs) {
+var scan = function (tx, callback) {
+  getDatum({transactions: [tx]}, function (err, txs) {
     var scannedTx = {
       data: txs[0]
     }
-    callback(err, scannedTx);
-  });
+    callback(err, scannedTx)
+  })
 }
 
 var rawOpReturn = {
@@ -112,4 +115,4 @@ var rawOpReturn = {
   scan: scan
 }
 
-module.exports = rawOpReturn;
+module.exports = rawOpReturn
--- test/index-spec.js
+++ test/index-spec.js
@@ -1,149 +1,205 @@
-jasmine.getEnv().defaultTimeoutInterval = 50000;
+/* global jasmine expect it describe */
 
-var rawOpReturn = require("../src/index");
+jasmine.getEnv().defaultTimeoutInterval = 50000
 
-var commonBlockchain;
-if (process.env.CHAIN_API_KEY_ID && process.env.CHAIN_API_KEY_SECRET) {
-  var ChainAPI = require("chain-unofficial");
-  commonBlockchain = ChainAPI({
-    network: "testnet", 
-    key: process.env.CHAIN_API_KEY_ID, 
-    secret: process.env.CHAIN_API_KEY_SECRET
-  });
-}
-else {
-  commonBlockchain = require("mem-common-blockchain")();
-}
+var rawOpReturn = require('../src/index')
 
-var bitcoin = require("bitcoinjs-lib");
+var commonBlockchain = require('mem-common-blockchain')()
 
-var randomString = function(length) {
-  var characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-  var output = '';
+var randomString = function (length) {
+  var characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
+  var output = ''
   for (var i = 0; i < length; i++) {
-    var r = Math.floor(Math.random() * characters.length);
-    output += characters.substring(r, r + 1);
+    var r = Math.floor(Math.random() * characters.length)
+    output += characters.substring(r, r + 1)
   }
-  return output;
-};
-
-var randomHex = function(length) {
-  var characters = "0123456789abcdef";
-  var output = '';
-  for (var i = 0; i < length; i++) {
-    var r = Math.floor(Math.random() * characters.length);
-    output += characters.substring(r, r + 1);
-  }
-  return output;
-};
-
-var seed = bitcoin.crypto.sha256("test");
-var wallet = new bitcoin.Wallet(seed, bitcoin.networks.testnet);
-var address = wallet.generateAddress();
-
-var signRawTransaction = function(txHex, cb) {
-  var tx = bitcoin.Transaction.fromHex(txHex);
-  var signedTx = wallet.signWith(tx, [address]);
-  var txid = signedTx.getId();
-  var signedTxHex = signedTx.toHex();
-  cb(false, signedTxHex, txid);
-};
-
-var commonWallet = {
-  signRawTransaction: signRawTransaction,
-  address: address
+  return output
 }
 
-describe("raw-op-return", function() {
+var randomHex = function (length) {
+  var characters = '0123456789abcdef'
+  var output = ''
+  for (var i = 0; i < length; i++) {
+    var r = Math.floor(Math.random() * characters.length)
+    output += characters.substring(r, r + 1)
+  }
+  return output
+}
 
-  it("should post a random string of 40 bytes as data", function(done) {
-    var data = new Buffer(randomString(40));
+var testCommonWallet = require('test-common-wallet')
+
+var commonWallet = testCommonWallet({
+  seed: 'test',
+  network: 'testnet',
+  commonBlockchain: commonBlockchain
+})
+
+describe('raw-op-return', function () {
+  it('should post a random string of 80 bytes as data', function (done) {
+    var data = new Buffer(randomString(80))
     rawOpReturn.post({
       data: data,
       commonBlockchain: commonBlockchain,
       commonWallet: commonWallet
-    }, function(error, postedTx) {
-      expect(postedTx.txid).toBeDefined();
-      console.log("sleeping for 1.5 seconds...");
-      setTimeout(function() {
-        commonBlockchain.Transactions.Get([postedTx.txid], function(err, txs) {
-          var tx = txs[0];
-          rawOpReturn.scan(tx, function(err, scannedTx) {
-            expect(scannedTx.data.toString('utf8')).toBe(data.toString('utf8'));
-            done();
-          });
-        });
-      }, 1500);
+    }, function (error, postedTx) {
+      if (error) {}
+      commonBlockchain.Transactions.Get([postedTx.txid], function (err, txs) {
+        if (err) {}
+        var tx = txs[0]
+        rawOpReturn.scan(tx, function (err, scannedTx) {
+          if (err) {}
+          expect(scannedTx.data.toString('utf8')).toBe(data.toString('utf8'))
+          done()
+        })
+      })
+    })
+  })
 
-    });
-  });
-
-  it("should not post more than 40 bytes of data", function(done) {
-    var data = new Buffer(randomString(41));
+  it('should post a random string of 79 bytes as data', function (done) {
+    var data = new Buffer(randomString(79))
     rawOpReturn.post({
       data: data,
       commonBlockchain: commonBlockchain,
       commonWallet: commonWallet
-    }, function(error, postedTx) {
-      expect(error).toBe("too large");
-      expect(postedTx).toBe(false);
-      done();
-    });
-  });
+    }, function (error, postedTx) {
+      if (error) {}
+      commonBlockchain.Transactions.Get([postedTx.txid], function (err, txs) {
+        if (err) {}
+        var tx = txs[0]
+        rawOpReturn.scan(tx, function (err, scannedTx) {
+          if (err) {}
+          expect(scannedTx.data.toString('utf8')).toBe(data.toString('utf8'))
+          done()
+        })
+      })
+    })
+  })
 
-  it("should not post less than 1 byte of data", function(done) {
-    var data = new Buffer(0);
+  it('should post a random string of 40 bytes as data', function (done) {
+    var data = new Buffer(randomString(40))
     rawOpReturn.post({
       data: data,
       commonBlockchain: commonBlockchain,
       commonWallet: commonWallet
-    }, function(error, postedTx) {
-      expect(error).toBe("too small");
-      expect(postedTx).toBe(false);
-      done();
-    });
-  });
+    }, function (error, postedTx) {
+      if (error) {}
+      commonBlockchain.Transactions.Get([postedTx.txid], function (err, txs) {
+        if (err) {}
+        var tx = txs[0]
+        rawOpReturn.scan(tx, function (err, scannedTx) {
+          if (err) {}
+          expect(scannedTx.data.toString('utf8')).toBe(data.toString('utf8'))
+          done()
+        })
+      })
+    })
+  })
 
-  it("should post as hex", function(done) {
-    var hexData = randomHex(8);
+  it('should post a random string of 39 bytes as data', function (done) {
+    var data = new Buffer(randomString(39))
+    rawOpReturn.post({
+      data: data,
+      commonBlockchain: commonBlockchain,
+      commonWallet: commonWallet
+    }, function (error, postedTx) {
+      if (error) {}
+      commonBlockchain.Transactions.Get([postedTx.txid], function (err, txs) {
+        if (err) {}
+        var tx = txs[0]
+        rawOpReturn.scan(tx, function (err, scannedTx) {
+          if (err) {}
+          expect(scannedTx.data.toString('utf8')).toBe(data.toString('utf8'))
+          done()
+        })
+      })
+    })
+  })
+
+  it('should post a random string of 41 bytes as data', function (done) {
+    var data = new Buffer(randomString(41))
+    rawOpReturn.post({
+      data: data,
+      commonBlockchain: commonBlockchain,
+      commonWallet: commonWallet
+    }, function (error, postedTx) {
+      if (error) {}
+      commonBlockchain.Transactions.Get([postedTx.txid], function (err, txs) {
+        if (err) {}
+        var tx = txs[0]
+        rawOpReturn.scan(tx, function (err, scannedTx) {
+          if (err) {}
+          expect(scannedTx.data.toString('utf8')).toBe(data.toString('utf8'))
+          done()
+        })
+      })
+    })
+  })
+
+  it('should not post more than 80 bytes of data', function (done) {
+    var data = new Buffer(randomString(81))
+    rawOpReturn.post({
+      data: data,
+      commonBlockchain: commonBlockchain,
+      commonWallet: commonWallet
+    }, function (error, postedTx) {
+      expect(error).toBe('too large')
+      expect(postedTx).toBe(false)
+      done()
+    })
+  })
+
+  it('should not post less than 1 byte of data', function (done) {
+    var data = new Buffer(0)
+    rawOpReturn.post({
+      data: data,
+      commonBlockchain: commonBlockchain,
+      commonWallet: commonWallet
+    }, function (error, postedTx) {
+      expect(error).toBe('too small')
+      expect(postedTx).toBe(false)
+      done()
+    })
+  })
+
+  it('should post as hex', function (done) {
+    var hexData = randomHex(8)
     rawOpReturn.post({
       hexData: hexData,
       commonBlockchain: commonBlockchain,
       commonWallet: commonWallet
-    }, function(error, postedTx) {
-      expect(postedTx.txid).toBeDefined();
-      console.log("sleeping for 1.5 seconds...");
-      setTimeout(function() {
-        commonBlockchain.Transactions.Get([postedTx.txid], function(err, txs) {
-          var tx = txs[0];
-          rawOpReturn.scan(tx, function(err, scannedTx) {
-            expect(scannedTx.data.toString('hex')).toBe(hexData);
-            done();
-          });
-        });
-      }, 1500);
-    });
-  });
+    }, function (error, postedTx) {
+      if (error) {}
+      expect(postedTx.txid).toBeDefined()
+      commonBlockchain.Transactions.Get([postedTx.txid], function (err, txs) {
+        if (err) {}
+        var tx = txs[0]
+        rawOpReturn.scan(tx, function (err, scannedTx) {
+          if (err) {}
+          expect(scannedTx.data.toString('hex')).toBe(hexData)
+          done()
+        })
+      })
+    })
+  })
 
-  it("should post as string", function(done) {
-    var stringData = randomString(8);
+  it('should post as string', function (done) {
+    var stringData = randomString(8)
     rawOpReturn.post({
       stringData: stringData,
       commonBlockchain: commonBlockchain,
       commonWallet: commonWallet
-    }, function(error, postedTx) {
-      expect(postedTx.txid).toBeDefined();
-      console.log("sleeping for 1.5 seconds...");
-      setTimeout(function() {
-        commonBlockchain.Transactions.Get([postedTx.txid], function(err, txs) {
-          var tx = txs[0];
-          rawOpReturn.scan(tx, function(err, scannedTx) {
-            expect(scannedTx.data.toString('utf8')).toBe(stringData);
-            done();
-          });
-        });
-      }, 1500);
-    });
-  });
-
-});
+    }, function (error, postedTx) {
+      if (error) {}
+      expect(postedTx.txid).toBeDefined()
+      commonBlockchain.Transactions.Get([postedTx.txid], function (err, txs) {
+        if (err) {}
+        var tx = txs[0]
+        rawOpReturn.scan(tx, function (err, scannedTx) {
+          if (err) {}
+          expect(scannedTx.data.toString('utf8')).toBe(stringData)
+          done()
+        })
+      })
+    })
+  })
+})
